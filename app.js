c7481995ecf9bbe030f8a53303d9f093c601a6ec
WinWinMaw4 <mawinwinmaw4@gmail.com>
2021-11-26 19:38:50 +0630

    1.0

---
+++ app.js
@@ -0,0 +1,53 @@
+let lightMode = document.getElementById("light-mode");
+let container = document.querySelector(".container");
+let mobileMenu = document.querySelector("#mobile-menu");
+let sideBar = document.querySelector(".sidebar");
+
+// localstorage
+// window.addEventListener("load",()=>{
+    let changed = localStorage.getItem("mode");
+    if(changed != "light"){
+        setDarkMode();
+    }else{
+        setLightMode();
+    }
+
+// })
+
+function setDarkMode(){
+    container.classList.remove("lightBackground");
+    container.classList.add("darkBackground");
+    document.querySelectorAll('.inverted').forEach((result)=>{
+        result.classList.remove('invert');
+    });
+    localStorage.setItem("mode","dark");
+}
+function setLightMode(){
+    container.classList.remove("darkBackground");
+    container.classList.add("lightBackground");
+    document.querySelectorAll('.inverted').forEach((result)=>{
+        result.classList.add('invert');
+    });
+    localStorage.setItem("mode","light")
+  
+}
+
+lightMode.addEventListener('click',()=>{
+    let darkMode = localStorage.getItem("mode");
+
+    if(darkMode === 'light'){
+        setDarkMode();
+      
+    }else{
+        setLightMode();       
+    }
+    // container.classList.toggle("lightBackground");
+    // container.classList.toggle("darkBackground");
+    // localStorage.setItem("mode","1");
+})
+// end localstoreage
+
+mobileMenu.addEventListener("click",()=>{
+    console.log(sideBar);
+    sideBar.classList.toggle("show");
+})
